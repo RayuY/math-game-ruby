7aa02ee7e193d6194e81b256c4c1ffbe9ad9bd51
Ray Yu <kaito-akira@hotmail.com>
2022-11-14 13:43:45 -0800

    Add question class

---
+++ Question.rb
@@ -0,0 +1,20 @@
+class Question
+
+  attr_reader : right_answer
+
+  def initialize()
+    @num_one = rand(1..20).to_i
+    @num_two = rand(1..20).to_i
+    @right_answer = @num_one + @num_two
+  end
+
+  def ask_question 
+    "What is #{@num_one} + #{@num_two}"
+  end
+
+  def correct?(answer)
+    answer == @right_answer
+  end
+
+end
+
